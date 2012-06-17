738ef623fc13ed453bf1d2b51e42ebab827f8390
Warner Godfrey <warner@warnergodfrey.com>
2012-06-17 19:04:35 +1000

    fixed syntax error

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 bash "install-railsready" do
   user node[:railsready][:install][:user]
   code <<-EOH
-    wget -P /tmp https://raw.github.com/joshfng/railsready/master/railsready.sh"
+    wget -P /tmp https://raw.github.com/joshfng/railsready/master/railsready.sh
     cat 1 | bash /tmp/railsready.sh
     rm /tmp/railsready.sh
   EOH
