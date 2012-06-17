dacc73ae863e4b5483c239c1cdb5692400093043
Warner Godfrey <warner@warnergodfrey.com>
2012-06-17 19:02:03 +1000

    Intial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Warner Godfrey"
+maintainer_email "w@warnergodfrey.com"
+license          "Apache 2.0"
+description      "Installs rails environment"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
