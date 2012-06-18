424afad41801e5c1781d8d3ba5bd656f855984be
Warner Godfrey <warner@warnergodfrey.com>
2012-06-18 17:44:03 +1000

    fixed input to railsready command

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,7 +18,7 @@ bash "install-railsready" do
   user node[:railsready][:install][:user]
   code <<-EOH
     wget -P /tmp https://raw.github.com/joshfng/railsready/master/railsready.sh &&
-    cat 1 | bash /tmp/railsready.sh &&
+    echo 1 | bash /tmp/railsready.sh &&
     rm /tmp/railsready.sh
   EOH
   action :run
