f9bc79cd2c98f1c837ae77c0207d6361d9d0bcb3
Warner Godfrey <warner@warnergodfrey.com>
2012-06-19 15:16:40 +1000

    user's home directory was not created

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,14 @@
+directory "/home/#{node[:railsready][:install][:user]}" do
+  owner node[:railsready][:install][:user]
+  group "admin"
+  mode "0755"
+  action :create
+end
+
 user "railsready" do
   username node[:railsready][:install][:user]
   group "admin"
-  home "/home/railsready"
+  home "/home/#{node[:railsready][:install][:user]}"
   shell "/bin/bash"
   comment "Temporary railsready install user"
   action :create
@@ -9,10 +16,10 @@ end
 
 bash "install-railsready" do
   user node[:railsready][:install][:user]
+  cwd "/home/#{node[:railsready][:install][:user]}" 
   code <<-EOH
     wget https://raw.github.com/joshfng/railsready/master/railsready.sh &&
-    echo 1 | bash railsready.sh &&
-    rm railsready.sh
+    echo 1 | bash railsready.sh
   EOH
   action :run
 end
@@ -21,3 +28,8 @@ user "railsready" do
   username node[:railsready][:install][:user]
   action :remove
 end
+
+directory "/home/#{node[:railsready][:install][:user]}" do
+  action :delete
+end
+
