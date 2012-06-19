6d6af53f75e75d8f59a2dd0d92d3b6b67a20e173
Warner Godfrey <warner@warnergodfrey.com>
2012-06-19 10:06:11 +1000

    removed sudo cookbook and reverted to adding railsready user to the admin group

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,16 +2,10 @@ include_recipe "sudo"
 
 user "railsready" do
   username node[:railsready][:install][:user]
+  group "admin"
   comment "Temporary railsready install user"
   password "$6$eaxpjwDMs$QI1mPT9zw23V38P3W3A/0Wk.nDgfcpCRoAY53445zKq7BSx5F0uxZYp7HnirlW2NBPcDuAV8y6WRbZEGy5j2A."
   action :create
-end
-
-sudo "railsready" do
-  user node[:railsready][:install][:user]
-  commands ["/tmp/railsready.sh"]
-  host "ALL"
-  nopasswd true
 end
 
 bash "install-railsready" do
@@ -28,8 +22,3 @@ user "railsready" do
   username node[:railsready][:install][:user]
   action :remove
 end
-
-File "/etc/sudoers.d/railsready" do
-  action :delete
-end
-
