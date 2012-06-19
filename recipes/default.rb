24edc5ccad99a6bdec27717cf5016c19bd0f1682
Warner Godfrey <warner@warnergodfrey.com>
2012-06-19 15:08:12 +1000

    removing password

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,6 @@ user "railsready" do
   home "/home/railsready"
   shell "/bin/bash"
   comment "Temporary railsready install user"
-  password "$6$eaxpjwDMs$QI1mPT9zw23V38P3W3A/0Wk.nDgfcpCRoAY53445zKq7BSx5F0uxZYp7HnirlW2NBPcDuAV8y6WRbZEGy5j2A."
   action :create
 end
 
