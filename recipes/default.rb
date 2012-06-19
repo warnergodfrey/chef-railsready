85333ec47735aec97b38e2bc536a311b1560179a
Warner Godfrey <warner@warnergodfrey.com>
2012-06-19 15:06:56 +1000

    railsready install user now has their own home directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,8 @@
 user "railsready" do
   username node[:railsready][:install][:user]
   group "admin"
+  home "/home/railsready"
+  shell "/bin/bash"
   comment "Temporary railsready install user"
   password "$6$eaxpjwDMs$QI1mPT9zw23V38P3W3A/0Wk.nDgfcpCRoAY53445zKq7BSx5F0uxZYp7HnirlW2NBPcDuAV8y6WRbZEGy5j2A."
   action :create
@@ -9,9 +11,9 @@ end
 bash "install-railsready" do
   user node[:railsready][:install][:user]
   code <<-EOH
-    wget -P /tmp https://raw.github.com/joshfng/railsready/master/railsready.sh &&
-    echo 1 | bash /tmp/railsready.sh &&
-    rm /tmp/railsready.sh
+    wget https://raw.github.com/joshfng/railsready/master/railsready.sh &&
+    echo 1 | bash railsready.sh &&
+    rm railsready.sh
   EOH
   action :run
 end
